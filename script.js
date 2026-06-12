584ebcd8a383f5c967237c4f12d4e6445bfdc377
Eduardo <94714636+CamposEdu@users.noreply.github.com>
2026-06-11 21:04:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
     validateConfig();
 
     // Set texts from config
-    document.getElementById('valentineTitle').textContent = `${config.valentineName}, meu amõ`;
+    document.getElementById('valentineTitle').textContent = `${config.valentineName}`;
     
     // Set first question texts
     document.getElementById('question1Text').textContent = config.questions.first.text;
