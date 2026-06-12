d7df6df57f0f6ab6105b3b0fc825ce5ab7e990b0
Eduardo <94714636+CamposEdu@users.noreply.github.com>
2026-06-11 21:17:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://files.catbox.moe/15r1pn.mp3", // Music streaming URL
+        musicUrl: "./Goo Goo Dolls - Iris - LatinHype (youtube).mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
